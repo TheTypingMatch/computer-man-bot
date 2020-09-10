1bbcf94e2dd82a683a18e9ce2371035d8a4af53d
Ryan Harrington <rharrington.web@gmail.com>
2020-09-10 12:48:21 -0600

    fix: updated help info 3

--- src/config/embeds.ts
+++ src/config/embeds.ts
@@ -1,6 +1,6 @@
 const helpInfo = {
     desc: 'An extention of the human intellect.',
-    descInfo: 'Star me on [**GitHub**](https://github.com/TheTypingMatch/computer-man-bot)!\n\n`+help server`\n`+help misc',
+    descInfo: 'Star me on [**GitHub**](https://github.com/TheTypingMatch/computer-man-bot)!\n\n`+help server`\n`+help misc`',
     server: {
         verify: ' - Register for TheTypingMatch server.'
         vote: ' - Upvote TheTypingMatch Discord server.'
