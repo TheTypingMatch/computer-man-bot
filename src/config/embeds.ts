4e8be3327de6b9f1e8a199603c53f736ef05e802
Ryan Harrington <rharrington.web@gmail.com>
2020-09-10 12:49:49 -0600

    fix: missing commas in object list

--- src/config/embeds.ts
+++ src/config/embeds.ts
@@ -2,8 +2,8 @@ const helpInfo = {
     desc: 'An extention of the human intellect.',
     descInfo: 'Star me on [**GitHub**](https://github.com/TheTypingMatch/computer-man-bot)!\n\n`+help server`\n`+help misc`',
     server: {
-        verify: ' - Register for TheTypingMatch server.'
-        vote: ' - Upvote TheTypingMatch Discord server.'
+        verify: ' - Register for TheTypingMatch server.',
+        vote: ' - Upvote TheTypingMatch Discord server.',
         rank: ' - Show your server activity.',
         leaderboard: ' - Display the most active users.'
     },
