7cf0004e0eaf0fde0c9ec1cd088f99cc4b25a2ad
Ryan Harrington <rharrington.web@gmail.com>
2020-09-10 12:46:51 -0600

    fix: updated help info

--- src/config/embeds.ts
+++ src/config/embeds.ts
@@ -1,10 +1,14 @@
 const helpInfo = {
     desc: 'An extention of the human intellect.',
     descInfo: 'Star me on [**GitHub**](https://github.com/TheTypingMatch/computer-man-bot)!\n\n`+help Stats`',
-    stats: {
-        ping: ' - Show bot latency.',
+    server: {
+        verify: ' - Register for TheTypingMatch server.'
+        vote: ' - Upvote TheTypingMatch Discord server.'
         rank: ' - Show your server activity.',
         leaderboard: ' - Display the most active users.'
+    },
+    misc: {
+        ping: ' - Show bot latency.',
     }
 };
 
